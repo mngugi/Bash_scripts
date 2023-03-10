4f4d312e9d64a6354ca90083cfaf789327e0bf38
M.ngugi <mngugi7@gmail.com>
2023-03-10 10:56:41 +0300

    Add files via upload

---
+++ telnet.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Prompt the user for input
+echo "Enter connection IP:"
+read IP
+
+# Send an HTTP GET request to the specified IP address using telnet
+(
+echo "GET / HTTP/1.1"
+echo "Host: $IP"
+echo "Connection: close"
+echo ""
+sleep 1
+) | telnet $IP 80
+
