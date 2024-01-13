fb72621c9d838448b6577b64d1728d26aeb4bd0f
M.ngugi <mngugi7@gmail.com>
2024-01-14 00:18:20 +0300

    Add files via upload

---
+++ Install_GTK.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+sudo dnf install gtk3-devel gstreamer1-devel clutter-devel webkit2gtk3-devel libgda-devel gobject-introspection-devel
+sudo dnf install devhelp gtk3-devel-docs clutter-doc
