aa64fa6f6dfd09c409ab06862da759b84274dbc1
M.ngugi <mngugi7@gmail.com>
2024-02-14 08:00:40 -0500

    Update update.sh

--- update.sh
+++ update.sh
@@ -4,3 +4,7 @@ sudo dnf update
 sudo dnf upgrade
 
 sudo dnf --refresh update 
+sudo dnf install dnf-plugin-system-upgrade
+sudo dnf install dnf-plugin-system-upgrade
+sudo dnf system-upgrade download --releasever=39
+sudo dnf system-upgrade reboot
