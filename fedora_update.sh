950adb994b8a7d70f7f496a3ca97054e93d3f59b
M.Ngugi <mngugi7@gmail.com>
2024-08-31 10:39:43 -0400

    Update fedora_update.sh

--- fedora_update.sh
+++ fedora_update.sh
@@ -1,11 +1,13 @@
 
 #!/bin/bash
 # Author: Peter Mwangi Ngugi
-# fedora daily updates on terminal
+# Fedora daily updates on terminal
 # Update package lists
+
 sudo dnf update
 
 # Upgrade installed packages
 # Reboots the computer after installations
+
 sudo dnf upgrade
 sudo reboot 
