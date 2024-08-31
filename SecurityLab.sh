ed7cfee172849ec33f22c941eb2b6ec4ba02e2ad
M.Ngugi <mngugi7@gmail.com>
2024-08-31 14:14:59 -0400

    Update SecurityLab.sh

--- SecurityLab.sh
+++ SecurityLab.sh
@@ -1,4 +1,27 @@
 #!/bin/bash
 
 # this is bash script programm to install a Security
-# lab on Fedora Os
+# lab on Fedora os
+
+# install EtherApe
+sudo dnf install etherape
+
+# install ettercap 
+
+# install wireshark
+
+# install medusa
+
+# install nmap
+
+# install scap-workbench
+
+# install skipfish
+
+# install sqlninja
+
+# install yersinia
+
+
+
+
