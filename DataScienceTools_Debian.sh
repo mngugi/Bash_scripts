c8e8c506a3af11eb14a401da5afbdd6febb257a8
M.Ngugi <mngugi7@gmail.com>
2024-09-13 03:00:43 +0300

    Update DataScienceTools_Debian.sh

--- DataScienceTools_Debian.sh
+++ DataScienceTools_Debian.sh
@@ -3,8 +3,8 @@
 # Update package list
 sudo apt update
 
-# Install pip
-sudo apt install python3-pip
+# Install required system packages
+sudo apt install -y python3-pip python3-gi python3-sphinx micropython
 
 # Create a virtual environment
 python3 -m venv myenv
@@ -12,81 +12,32 @@ python3 -m venv myenv
 # Activate the virtual environment
 source myenv/bin/activate
 
-# Install the Gekko optimization library
-pip install gekko
-
-# Install BeautifulSoup4 and lxml for web scraping
-pip install beautifulsoup4 lxml
-
-# Repeated installation of Gekko; you can remove this line as it's redundant
-pip install gekko
-
-# Install Keras for deep learning
-pip install keras
-
-# Install Matplotlib for data visualization
-pip install matplotlib
-
-# Install NumPy for numerical computations
-pip install numpy
-
-# Install OpenCV-Python for computer vision tasks
-pip install opencv-python
-
-# Install Pandas for data manipulation
-pip install pandas
-
-# Install Pandas Profiling for exploratory data analysis (with notebook support)
-pip install pandas-profiling[notebook]
-
-# Install Plotly for interactive data visualization
-pip install plotly
-
-# Install PyTorch for deep learning
-pip install torch
-
-# Install scikit-learn for machine learning tasks
-pip install scikit-learn
-
-# Install SciPy for scientific and technical computing
-pip install scipy
-
-# Install Seaborn for data visualization
-pip install seaborn
-
-# Install StatsModels for statistical modeling
-pip install statsmodels
-
-# Install the TcLab package
-pip install tclab
-
-# Install TensorFlow for deep learning
-pip install tensorflow
-
-# Install XGBoost for gradient boosting
-pip install xgboost
-
-# Install PyGobject
-sudo apt install python3-gi
-
-# Install Sphinx
-sudo apt install python3-sphinx
-
-# Install MicroPython
-sudo apt install micropython
-
-# Install Jupyter notebook
-pip install notebook
-
-# Install IPython
-pip install ipython
-
-# Install tox
-pip install tox
+# Install Python packages in the virtual environment
+pip install \
+  gekko \
+  beautifulsoup4 \
+  lxml \
+  keras \
+  matplotlib \
+  numpy \
+  opencv-python \
+  pandas \
+  pandas-profiling[notebook] \
+  plotly \
+  torch \
+  scikit-learn \
+  scipy \
+  seaborn \
+  statsmodels \
+  tclab \
+  tensorflow \
+  xgboost \
+  notebook \
+  ipython \
+  tox
 
 # List all installed packages and their versions
 pip list
 
 # Deactivate the virtual environment
 deactivate
-
