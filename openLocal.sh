7367ff19d26cff2004998e137a7492cca96cec4e
mngugi <mngugi7@gmail.com>
2024-10-18 19:19:37 +0300

    Update openLocal.sh

--- openLocal.sh
+++ openLocal.sh
@@ -4,7 +4,7 @@
 python3 -m http.server 8000 &
 
 # Give the server a second to start
-sleep 2
+sleep 6
 
 # Open the default web browser and navigate to http://localhost:8000
 xdg-open http://localhost:8000   # For Linux
