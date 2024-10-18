728c5cc0193d73331d29a22c7e35331a6bcf68e6
mngugi <mngugi7@gmail.com>
2024-10-18 19:18:26 +0300

    Update openLocal.sh

--- openLocal.sh
+++ openLocal.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 
-#http://localhost:8000
+# Start a simple HTTP server using Python in the background
+python3 -m http.server 8000 &
 
-#!/bin/bash
+# Give the server a second to start
+sleep 2
 
-# Start a simple HTTP server using Python
-python3 -m http.server 8000
+# Open the default web browser and navigate to http://localhost:8000
+xdg-open http://localhost:8000   # For Linux
+
+# For macOS, use the following line instead:
+# open http://localhost:8000
 
