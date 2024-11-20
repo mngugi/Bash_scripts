fb365373fe505fc46d0970d63e5d40b97e579341
M.Ngugi <mngugi7@gmail.com>
2024-11-20 23:44:00 +0300

    Update Install_GTK.sh

--- Install_GTK.sh
+++ Install_GTK.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+# Author : Peter Mwangi Ngugi
+
+
 
 sudo dnf install gtk3-devel gstreamer1-devel clutter-devel webkit2gtk3-devel libgda-devel gobject-introspection-devel
 sudo dnf install devhelp gtk3-devel-docs clutter-doc
