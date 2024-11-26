f23f822c6f242d74d4f0c1ae975c3edaa545941f
M.Ngugi <mngugi7@gmail.com>
2024-11-27 00:17:26 +0300

    Update memoryCheck.sh

--- memoryCheck.sh
+++ memoryCheck.sh
@@ -17,3 +17,7 @@ sar -r
 
 echo "Memory Infor:"
 cat /proc/meminfo  
+
+echo "How times the system loads."
+
+for log in `ls -1 /var/log/sa/sa[0-9]*`; do echo $log; sar -q -f $log | egrep -v “Average|ldavg” | awk ‘{if ($5>=1) print $1,$2,$5}';echo""; done | less
