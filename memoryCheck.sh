f924636dc7c49ab9d1dd55b393492faf5f7ad57b
M.Ngugi <mngugi7@gmail.com>
2024-11-27 00:21:36 +0300

    Update memoryCheck.sh

--- memoryCheck.sh
+++ memoryCheck.sh
@@ -17,12 +17,5 @@ sar -r  # Display memory statistics
 echo -e "\nDetailed Memory Information:"
 cat /proc/meminfo  
 
-echo -e "\nSystem Load Analysis:"
 
-# Loop through system activity logs and display high load instances
-for log in /var/log/sa/sa[0-9]*; do
-  echo "Log File: $log"
-  sar -q -f "$log" | egrep -v "Average|ldavg" | awk '{if ($5 >= 1) print $1, $2, $5}'
-  echo ""
-done | less
 
