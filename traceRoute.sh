7dd05c514267d6c8495738e61dd739b1c72befa8
M.Ngugi <mngugi7@gmail.com>
2025-01-07 17:11:15 +0300

    Update traceRoute.sh

--- traceRoute.sh
+++ traceRoute.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
 # Prompt user for IP address
+echo "====================================="
+echo "     Trace Route"
+echo "====================================="
 read -p "Enter IP address to trace: " ip_address
 
 # Perform traceroute
