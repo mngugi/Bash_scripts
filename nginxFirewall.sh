65e250db1d23c8db4530b8adb3dcbc9aeeb15fc6
M.Ngugi <mngugi7@gmail.com>
2025-01-07 16:53:20 +0300

    Update nginxFirewall.sh

--- nginxFirewall.sh
+++ nginxFirewall.sh
@@ -1,4 +1,7 @@
 #! /bin/bash
 # Author Peter Mwangi Ngugi
+echo "====================================="
+echo "     Firewall Add Service"
+echo "====================================="
 sudo firewall-cmd --permanent --add-service=http
 sudo firewall-cmd --permanent --add-service=https
