ebb546a12dc3f1ad9148e0d045ec67c907b069a2
M.Ngugi <mngugi7@gmail.com>
2025-01-07 16:10:33 +0300

    Update connectedProcesses.sh

--- connectedProcesses.sh
+++ connectedProcesses.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
+echo "====================================="
+echo "     System Operating Processes "
+echo "====================================="
 
 ps -xo comm | sort | uniq | grep -v sh | more
