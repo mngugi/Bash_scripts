f9511a0ed747e32383f2017bda9c5799109ae938
M.Ngugi <mngugi7@gmail.com>
2025-01-07 16:52:24 +0300

    Update Mu_Editor.sh

--- Mu_Editor.sh
+++ Mu_Editor.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 # Author : Peter Mwangi Ngugi
+echo "====================================="
+echo "    AppImage Tool"
+echo "====================================="
 
 QT_QPA_PLATFORM=wayland ./Mu_Editor-1.2.0-x86_64.AppImage
 
