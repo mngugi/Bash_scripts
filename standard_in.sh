13f6c1bfb56cb9a7f24bfc6f40c2a87cc984d8d4
M.Ngugi <mngugi7@gmail.com>
2025-01-07 17:04:32 +0300

    Update standard_in.sh

--- standard_in.sh
+++ standard_in.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/bash
-
+echo "====================================="
+echo "     Read File Path"
+echo "====================================="
 read -p 'File: ' FILENAME
 FPATH='readlink -f $FILENAME'
 echo 'Full path: ' $FPATH
