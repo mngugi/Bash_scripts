3b23748b55857348d2671eae19255513ad964a85
M.Ngugi <mngugi7@gmail.com>
2025-01-07 17:05:14 +0300

    Update standard_out.sh

--- standard_out.sh
+++ standard_out.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
-
+echo "====================================="
+echo "     Sort Names"
+echo "====================================="
 echo -e "Adidas\nNike\nPuma\nNew Balance\nUnder Armor" | sort
 echo -e "Adidas\nNike\nPuma\nNew Balance\nUnder Armor\nAdidas\nNike\nPuma\nNew Balance\nUnder Armor" | sort | uniq -c
 ps -ef | grep python 
