61c791634abc42e79b68a43621b4138c41308a7f
M.Ngugi <mngugi7@gmail.com>
2025-01-07 17:05:46 +0300

    Update startXamppLamp.sh

--- startXamppLamp.sh
+++ startXamppLamp.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
+echo "====================================="
+echo "     Start Lampp Server"
+echo "====================================="
 
 cd /opt/lampp && sudo ./manager-linux-x64.run
