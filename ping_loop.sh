474f493b55324321b49b26108d3c968df1667e70
M.Ngugi <mngugi7@gmail.com>
2025-01-07 16:54:44 +0300

    Update ping_loop.sh

--- ping_loop.sh
+++ ping_loop.sh
@@ -1,4 +1,7 @@
 #!/bin/bash 
+echo "====================================="
+echo "     Ping Ip"
+echo "====================================="
 
 for ip in $(seq 20 32); do
 ping -c 1 192.168.100.$ip |grep "bytes from" |cut " " -f 4 |cut -d":" -f1 
