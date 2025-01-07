b86945abd746d5c774a17277c612ae798df7a690
M.Ngugi <mngugi7@gmail.com>
2025-01-07 17:03:57 +0300

    Update sleep.sh

--- sleep.sh
+++ sleep.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/bash
-
+echo "====================================="
+echo "     Sleep!"
+echo "====================================="
 while true;
 	do echo "Draw will favor team g ...";
 	sleep 5;
