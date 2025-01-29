6dd645dbb247d5c33f0f16f5adfc4f6356e3874c
M.Ngugi <mngugi7@gmail.com>
2025-01-29 14:10:14 +0300

    Update installOllam.sh

--- installOllam.sh
+++ installOllam.sh
@@ -2,3 +2,11 @@
 
 sudo dnf install git
 git --version
+
+curl -fsSL https://ollama.com/install.sh | sh
+ollama --version
+ 
+sudo systemctl start ollama
+sudo systemctl enable ollama
+
+ollama run deepseek-r1:7b
