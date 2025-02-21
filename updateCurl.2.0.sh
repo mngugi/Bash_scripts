f258977018b3871a273436f21f10c8fa6e0e9916
M.Ngugi <mngugi7@gmail.com>
2025-02-21 22:36:28 +0300

    Update updateCurl.2.0.sh

--- updateCurl.2.0.sh
+++ updateCurl.2.0.sh
@@ -1,3 +1,4 @@
+  GNU nano 8.1                              updateCurl.2.0.sh                                         
 #!/bin/bash
 
 # Set the URL to request
@@ -12,10 +13,11 @@ response=$(curl -s -o /dev/null -w "%{http_code}" "$url")
 # Print the response code
 echo "Response code: $response"
 
-zenity --progress --title="url loading progress " --text="loading .." --pulsate --auto-close --auto-kill
+zenity --progress --title="url loading progress " --text="loading .." --pulsate --auto-close --auto-k>
 
 if [ $? -eq 0 ]; then
     zenity --info --title="Success" --text="url loading completed successfully!"
 else
-    zenity --error --title="Error" --text="Failed to load url . Please check your internet connection or package manager."
+    zenity --error --title="Error" --text="Failed to load url . Please check your internet connection>
 fi
+
