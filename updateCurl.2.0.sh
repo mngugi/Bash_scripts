a8e1f2b1a4bd9ce713c60e02faa7e8da5883cf25
M.Ngugi <mngugi7@gmail.com>
2025-02-21 22:29:07 +0300

    Create updateCurl.2.0.sh

---
+++ updateCurl.2.0.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Set the URL to request
+echo "====================================="
+echo "     Url Code"
+echo "====================================="
+url="https://github.com"
+
+# Make the request using curl
+response=$(curl -s -o /dev/null -w "%{http_code}" "$url")
+
+# Print the response code
+echo "Response code: $response"
+
+zenity --progress --title="url loading progress " --text="loading .." --pulsate --auto-close --auto-kill
+
