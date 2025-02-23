ec859d7251d7a63dbb78c1d43ef658cc21aaa590
M.Ngugi <mngugi7@gmail.com>
2025-02-23 12:00:11 +0300

    Update webDisplay.sh

--- webDisplay.sh
+++ webDisplay.sh
@@ -1,21 +1,24 @@
 #!/bin/sh
-# show web page in a dialog with a next step action
-#
+# Show a web page in a dialog with a next-step action
+
 theurl="https://developer.gnome.org"
- 
-zenity --text-info --title="Background Reading" --html --url=$theurl \
-       --checkbox="I read it...and I'm good to go"
+
+zenity --text --title="Background Reading" \
+       --text="Please visit: $theurl" \
+       --ok-label="I read it... and I'm good to go"
+
 rc=$?
-echo $rc
+
 case $rc in
     0)
         echo "Start some next step"
-    # next step
-    ;;
+        # Add the next step command here
+        ;;
     1)
         echo "Stop installation!"
-    ;;
+        ;;
    -1)
         echo "An unexpected error has occurred."
-    ;;
+        ;;
 esac
+
