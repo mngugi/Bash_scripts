5bf32f0d01ed7b0fe8068f491627fcf242969ec0
M.Ngugi <mngugi7@gmail.com>
2025-02-23 11:52:53 +0300

    Update cpuIdletime.sh

--- cpuIdletime.sh
+++ cpuIdletime.sh
@@ -1,6 +1,7 @@
 
 cpu_idle=$(top -bn1 | grep "Cpu(s)" | awk '{print $8}')
-zenity --info --text=" Cpu Idle time : cpu_idle%" --title="CPU Idle Time "
+zenity --info --text="Cpu Idle time : cpu_idle%" 
+
 
 
 
