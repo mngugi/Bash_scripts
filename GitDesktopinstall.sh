eb7648f4e712cbf00365030e3ae19ecfbbb32b02
P.M.Ngugi <mngugi7@gmail.com>
2025-09-04 20:50:16 +0300

    Create GitDesktopinstall.sh

---
+++ GitDesktopinstall.sh
@@ -0,0 +1,5 @@
+wget -qO - https://mirror.mwt.me/shiftkey-desktop/gpgkey | gpg --dearmor | sudo tee /usr/share/keyrings/mwt-desktop.gpg > /dev/null
+sudo sh -c 'echo "deb [arch=amd64 signed-by=/usr/share/keyrings/mwt-desktop.gpg] https://mirror.mwt.me/shiftkey-desktop/deb/ any main" > /etc/apt/sources.list.d/mwt-desktop.list'
+sudo apt update && sudo apt install github-desktop   #!/bin/bash
+
+
